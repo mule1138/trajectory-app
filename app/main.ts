dcecb3f6d87aca6c3c6dd7cee135a9fa2739596f
Kevin Mueller <>
2020-03-05 14:48:43 +0100

    Added index.html and main.ts

---
+++ app/main.ts
@@ -0,0 +1,23 @@
+import EsriMap from "esri/Map";
+import MapView from "esri/views/MapView";
+import SceneView from "esri/views/SceneView";
+import Camera from "esri/Camera";
+
+const map = new EsriMap({
+    basemap: "terrain",
+    ground: "world-elevation"
+});
+
+const view = new SceneView({
+    map: map,
+    container: "viewDiv",
+    camera: new Camera ({
+        position: {
+            longitude: -118.244,
+            latitude: 33.62,
+            z: 100000
+        },
+        heading: 0,
+        tilt: 20
+    }),
+});
